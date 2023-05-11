bbc870980904d669038c074e0c311bb3bb99095c
martianwei <martianwei456@gmail.com>
2023-05-11 23:17:52 +0800

    [New Feature] worker app

---
+++ reservation_worker_app/task.py
@@ -0,0 +1,20 @@
+from celery import Celery
+from db.postgresql import PostgresqlConnector
+from configs import configs
+
+postgresqlClient = PostgresqlConnector()
+
+celery_app = Celery(configs.APP_NAME,
+                    include=['webscaner.tasks'])
+
+celery_app.conf.update(
+    timezone='Asia/Taipei',  # 设置时区
+    enable_utc=False,  # 默认为true，UTC时区
+    broker_url=configs.CELERY_BROKER_URL,  # broker，注意rabbitMQ的VHOST要给你使用的用户加权限
+    result_backend=configs.CELERY_RESULT_BACKEND,  # backend配置，注意指定redis数据库
+    worker_concurrency=4,  # worker最大并发数
+    worker_max_tasks_per_child=100,  # 每个worker最多执行100个任务被销毁，可以防止内存泄漏
+    task_acks_late=True,  # 如果不设置，默认是celery队列，此处使用默认的直连交换机，routing_key完全一致才会调度到celery_demo队列
+    worker_prefetch_multiplier=1,
+    result_expires=3600,  # 任务结果过期时间
+)
