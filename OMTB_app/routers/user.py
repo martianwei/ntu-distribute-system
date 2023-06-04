3401d3176224dbfbd41f4a7e7c046582da338a22
martianwei <martianwei456@gmail.com>
2023-06-04 14:14:47 +0800

    delete dead code

--- OMTB_app/routers/user.py
+++ OMTB_app/routers/user.py
@@ -8,7 +8,4 @@ router = APIRouter()
 @router.get("/user/reservations", tags=["user"])
 async def get_reservation(user_id: int) -> Dict[str, list[ReservationSchema]]:
     reservations = get_reservation_by_user_id(user_id)
-    # print(reservations)
-    # return {"reservations": "good"}
     return {"reservations": reservations}
-# -> Dict[str, ReservationSchema]
